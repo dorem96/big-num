fda0857608ccf79dd30dd9664d772d20e3a25695
LeeJiSeon <j5119s@naver.com>
2016-06-07 21:04:34 +0900

    식 입력 및 재배열 (#10)
    * 식 입력 및 재배열
    
    식을 문자열로 입력받은 후 그 식을 숫자와 연산으로 분리하여 이차원 배열에 저장한다.
    그 다음에 숫자들을 정수와 소수부분을 분리하여 또 다른 일차원 배열에 저장한다.
    
    * 입력및재배열
    
    식입력 -> 이차원배열에 수와 연산 분리 -> 숫자만 따로 추출 -> 숫자 일차원배열(b배열) 에 저장 (이 때 소숫점이 51번째에 오도록, 정수만 있을때는 정수가 49번째부터 저장되게) -> 일차원배열에 있는 것을 다시 이차원배열로 이동

---
+++ 재배열.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+int main()
+{
+  char str[10000];
+  char a[1000][60] = {0};
+  char b[60] = {0};
+  
+  int i = 0, p = 0, q = 0, x = 49;
+  
+  scanf("%s", str);
+  
+  for(;i < 10000;i++)
+  {
+    if(str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+    {
+      a[++p][0] = str[i];
+      p++;
+      q = 0;
+    }
+    else
+    {
+      a[p][q] = str[i];
+      q++;
+    }
+  }
+  
+  for(p = 0;p < 1000;p++)
+  {
+    if(p%2 == 0 && p == 0)
+    {
+      char b[60] = {0};
+      x = 49;
+      
+      for(q = 0;q < 60;q++)
+      {
+        if(a[p][q] == '.')
+        {
+          b[50] = '.';
+          
+          for(int r = q-1, x = 49;r >= 0;r--)
+          {
+            b[x] = a[p][r];
+            x--;
+          }
+          for(int s = q+1, x = 51;a[p][s] != 0;s++)
+          {
+            b[x] = a[p][s];
+            x++;
+          }
+          break;
+        }
+        else
+        {
+          while(x >= 0)
+          {
+            if(a[p][59-q] != 0)
+            {
+              b[x] = a[p][59-q];
+              x--;
+              break;
+            }
+            else
+              break;
+          }
+        }
+      }
+      for(int m = 0;m < 60;m++)
+        a[p][m] = b[m];
+    }
+    else
+      continue;
+  }
+  
+  return 0;
+}
