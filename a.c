183dc983caacdf0af70ceba8c4c50d1517d1dbae
LeeJuhae <tlatmsdlwngo@naver.com>
2016-06-08 17:52:01 +0900

    Update a.c

--- a.c
+++ a.c
@@ -41,7 +41,7 @@ int main()
 				k++;
 			}
 		}
-		else if(str[i] == '.' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9' || str[i] == '0')//숫자를 입력했을 때
+		else if(str[i] == '.' || ((str[i] >= '0')&&(str[i] <= '9')))//숫자를 입력했을 때
 		{
 			a[p][q] = str[i];
 			q++;
